7e9219cd177600bdd7da24d3a81eeead8be1f0a2
stevepapazis <stevepapazis@users.noreply.github.com>
2022-12-10 01:40:39 +0200

    Revert "initial draft"
    This reverts commit a363993aea4010bfbe57b7b10217252793305ff4.

--- src/optimization/__init__.py
+++
@@ -1,9 +0,0 @@
-from __future__ import annotations
-__all__ = []
-
-
-from .algorithms import *
-from .dataset import *
-from .math_machinery import *
-from .test_functions import *
-from .visualization import *
